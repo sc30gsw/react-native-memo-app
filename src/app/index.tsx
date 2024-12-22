c54870f3751cb64185ca1fa52ec8b2f7f8c47664
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-22 15:03:12 +0900

    💄 メモ一覧画面のマークアップ

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,32 @@ import { Text } from "react-native";
 
 const Home = () => {
   return (
-    <View className="min-h-dvh w-dvw flex flex-col items-center justify-center">
-      <Text className="text-2xl">Home</Text>
+    <View className="flex-1 justify-center items-center">
+      {/* ヘッダー */}
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* メモリスト */}
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* 追加ボタン */}
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 }
