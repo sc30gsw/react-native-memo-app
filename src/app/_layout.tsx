ba9317dca3890804a4b341640206a770aa8c68e7
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-22 14:56:55 +0900

    🚑 expo-router等のセットアップ完了

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Slot } from "expo-router";
+
+import "../../global.css";
+
+
+const Layout = () => {
+  return (
+    <Slot />
+  )
+}
+
+
+export default Layout
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,11 +3,8 @@ import { Text } from "react-native";
 
 const Home = () => {
   return (
-    <View className="flex-1 items-center p-6 bg-slate-500">
-      <View className="flex-1 justify-center max-w-[960px] mx-auto">
-        <Text className="text-6xl font-bold text-red-500">Hello World</Text>
-        <Text className="text-3xl text-[#38434D]">This is the first page of your app.</Text>
-      </View>
+    <View className="min-h-dvh w-dvw flex flex-col items-center justify-center">
+      <Text className="text-2xl">Home</Text>
     </View>
   );
 }
