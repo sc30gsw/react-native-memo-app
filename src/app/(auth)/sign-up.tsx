7f04bce4a57145e6443f0fd890f030eafcbc89cd
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-26 17:57:23 +0900

    💄 会員登録画面の作成

---
+++ src/app/(auth)/sign-up.tsx
@@ -0,0 +1,34 @@
+import { Header } from '@/components/header'
+import React from 'react'
+import { Text, TextInput, View } from 'react-native'
+
+const SignUp = () => {
+  return (
+    <View className="flex-1 bg-slate-200">
+      <Header />
+      <View className="px-7 py-6 flex-col gap-4">
+        <Text className="text-2xl font-bold">Sign Up</Text>
+
+        <TextInput
+          placeholder="Email Address"
+          className="border border-gray-300 bg-white h-12 p-2 text-base rounded-md"
+        />
+        <TextInput
+          placeholder="Password"
+          className="border border-gray-300 bg-white h-12 p-2 text-base rounded-md"
+        />
+
+        <View className="bg-blue-500 rounded-md self-start">
+          <Text className="text-base text-white px-6 py-2">Submit</Text>
+        </View>
+
+        <View className="flex-row">
+          <Text className="text-sm">Already registered?</Text>
+          <Text className="text-sm text-blue-500 ml-2">Log in</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default SignUp
