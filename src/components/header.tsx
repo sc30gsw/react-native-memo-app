350acf303b565e49dbaa25981463ecfb3692b2f6
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-01-11 14:18:23 +0900

    Revert "🚸 詳細画面のときに一覧に戻れるようUI追加"
    This reverts commit 3e63ef7cda49ff9115c9a8004739b379f6f5b349.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,12 @@
-import { cn } from '@/utils/classes'
-import { MaterialIcons } from '@expo/vector-icons'
-import { router, usePathname } from 'expo-router'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 export const Header = () => {
-  const pathname = usePathname()
-  const isMemoDetail = pathname === '/memo/detail'
-
   return (
-    <View
-      className={cn(
-        'bg-blue-500 h-[104px] justify-center items-center pt-10',
-        isMemoDetail && 'flex-row justify-between px-4',
-      )}
-    >
-      {isMemoDetail && (
-        <TouchableOpacity onPress={() => router.back()}>
-          <MaterialIcons name="arrow-back-ios" size={24} color="white" />
-        </TouchableOpacity>
-      )}
-      <View>
+    <View className="bg-blue-500 h-[104px] justify-end items-center">
+      <View className="mb-2">
         <Text className="text-2xl font-bold text-white">Memo App</Text>
       </View>
-      <View className={isMemoDetail ? '' : 'absolute bottom-7 right-4'}>
+      <View className="absolute bottom-4 right-4">
         <Text className="text-white opacity-70">ログアウト</Text>
       </View>
     </View>
