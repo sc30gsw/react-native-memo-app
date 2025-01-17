a201e9b67aa019c200004ea959feb0432cb3c4ee
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-01-18 06:15:18 +0900

    ✨ ログイン機能の実装

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,6 +1,9 @@
 import { AuthForm } from '@/features/auth/components/auth-form'
+import { auth } from '@/libs/config'
 import { router } from 'expo-router'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import React from 'react'
+import { Alert } from 'react-native'
 
 const Login = () => {
   return (
@@ -8,9 +11,22 @@ const Login = () => {
       title="Log In"
       navigationText="Not registered?"
       navigationLink="Sign up here!"
-      onPress={() => {
-        // TODO: login
-        router.replace('/memo/list')
+      onPress={(data) => {
+        signInWithEmailAndPassword(auth, data.email, data.password)
+          .then(() => {
+            router.replace('/memo/list')
+          })
+          .catch((err: unknown) => {
+            if (err instanceof Error) {
+              Alert.alert(
+                'Email or Password is incorrect. \n Please try again.',
+              )
+
+              return
+            }
+
+            Alert.alert('Something went wrong')
+          })
       }}
     />
   )
