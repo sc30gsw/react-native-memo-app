6f7177c4f3b5477f8b2d89dd226fbe4746718b4a
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-01-18 06:29:20 +0900

    💄  ローディング表示

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,28 @@
 import { auth } from '@/libs/config'
 import { Redirect, router } from 'expo-router'
 import { onAuthStateChanged } from 'firebase/auth'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { View } from 'react-native'
 
 const Home = () => {
+  const [isPending, setIsPending] = useState(true)
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         router.replace('/memo/list')
       }
+      setIsPending(false)
     })
   }, [])
+
+  if (isPending) {
+    return (
+      <View className="flex items-center my-4">
+        <View className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent" />
+      </View>
+    )
+  }
 
   return <Redirect href={'/login'} />
 }
