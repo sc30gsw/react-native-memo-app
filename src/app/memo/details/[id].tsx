8a0348ca721c7460a33332358a0badadaa31709e
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-01-18 19:10:31 +0900

    ✨ 詳細データ取得機能の実装

--- src/app/memo/details/[id].tsx
+++ src/app/memo/details/[id].tsx
@@ -1,27 +1,78 @@
+import { Spinner } from '@/components/ui/spinner'
+import { MEMO_CACHE_KEY } from '@/constants'
 import { CircleButton } from '@/features/memo/components/circle-button'
+import { Memo } from '@/features/memo/types/memo'
+import { auth } from '@/libs/config'
+import { fetcher } from '@/libs/fetcher'
 import { Foundation } from '@expo/vector-icons'
+import { useQuery } from '@tanstack/react-query'
+import { format } from 'date-fns'
+import { ja } from 'date-fns/locale/ja'
 import { router, useLocalSearchParams } from 'expo-router'
 import { ScrollView, Text, View } from 'react-native'
+
+const fetchMemo = async (memoId: string) => {
+  if (!auth.currentUser) {
+    router.replace('/login')
+
+    return
+  }
+
+  const memo = await fetcher<Memo>(
+    `users/${auth.currentUser.uid}/memos`,
+    undefined,
+    memoId,
+  )
+
+  if (!memo) {
+    router.back()
+
+    return
+  }
+
+  return memo
+}
 
 const MemoDetail = () => {
   const { id } = useLocalSearchParams()
 
+  const { data, isLoading, error } = useQuery({
+    queryKey: [`${MEMO_CACHE_KEY}/${id}`],
+    queryFn: () => fetchMemo(id as string),
+  })
+
+  if (isLoading || !data) {
+    return <Spinner />
+  }
+
+  if (error) {
+    return (
+      <View className="flex-1 items-center my-8">
+        <Text className="font-bold text-xl text-red-500">
+          Something went wrong.
+        </Text>
+      </View>
+    )
+  }
+
   return (
     <View className="flex-1 bg-white">
       <View className="bg-blue-500 h-24 justify-center py-6 px-[19px]">
-        <Text className="text-white text-xl leading-8 font-bold">
-          買い物リスト {id}
-        </Text>
-        <Text className="text-white text-xs">2023年10月1日 10:00</Text>
+        <View className="flex-1 gap-2  mr-2">
+          <Text className="text-white text-xl leading-8 font-bold truncate">
+            {data.content}
+          </Text>
+
+          <Text className="text-white text-xs">
+            {format(data.updatedAt.toDate(), 'yyyy年MM月dd日 HH:mm', {
+              locale: ja,
+            })}
+          </Text>
+        </View>
       </View>
 
       <ScrollView className="py-8 px-7">
-        <Text className="text-base">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
-          architecto consequuntur, aspernatur recusandae odit facilis expedita
-          saepe sequi, magnam alias, accusamus ullam consequatur hic aperiam
-          nostrum perspiciatis earum enim. Facilis.
-        </Text>
+        <Text className="text-base">{data.content}</Text>
       </ScrollView>
 
       <View className="flex-1 items-end justify-end">
--- src/libs/fetcher.ts
+++ src/libs/fetcher.ts
@@ -1,14 +1,28 @@
 import { db } from '@/libs/config'
-import { collection, getDocs, query } from 'firebase/firestore'
+import { collection, doc, getDoc, getDocs, query } from 'firebase/firestore'
 
 type QueryArgs = Parameters<typeof query>
 
 export const fetcher = async <T>(
   path: string,
-  queries: QueryArgs[1],
-): Promise<T> => {
+  queries?: QueryArgs[1],
+  docId?: string,
+): Promise<T | null> => {
+  if (docId) {
+    const ref = doc(db, path, docId)
+    const snapshot = await getDoc(ref)
+
+    if (snapshot.exists()) {
+      const res = snapshot.data() as T
+
+      return res
+    }
+
+    return null
+  }
+
   const ref = collection(db, path)
-  const q = query(ref, queries)
+  const q = queries ? query(ref, queries) : ref
 
   const res = await getDocs(q)
 
