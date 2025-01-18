7e05c8924525899204b0ccd334b1fcbfb0034270
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-01-18 11:01:49 +0900

    💄 非活性時のスタイル変化を実装

--- src/features/memo/components/circle-button.tsx
+++ src/features/memo/components/circle-button.tsx
@@ -5,20 +5,24 @@ import { TouchableOpacity, View } from 'react-native'
 type CircleButtonProps = {
   icon: ReactNode
   onPress: () => void
+  disabled?: boolean
   className?: ComponentProps<typeof View>['className']
 }
 
 export const CircleButton = ({
   icon,
   onPress,
+  disabled,
   className,
 }: CircleButtonProps) => {
   return (
     <TouchableOpacity
       onPress={onPress}
+      disabled={disabled}
       className={cn(
         'size-16 rounded-full bg-blue-500 items-center justify-center absolute bottom-10 right-10 shadow-lg',
         className,
+        disabled && 'bg-neutral-300',
       )}
     >
       {icon}
